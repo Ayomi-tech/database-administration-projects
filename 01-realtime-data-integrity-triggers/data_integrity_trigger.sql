6979030c51b889ff161c90c2526f6b57c95a941f
Ayodeji <aayo.agunbiade@gmail.com>
2025-11-14 21:16:23 +0100

    Update Trigger File to more cleaner query

--- 01-realtime-data-integrity-triggers/data_integrity_trigger.sql
+++ 01-realtime-data-integrity-triggers/data_integrity_trigger.sql
@@ -5,15 +5,15 @@
 USE mavenbearbbuilders ;
 
 -- Creation of orders table 
-CREATE TABLE orders(
-order_id BIGINT ,
-created_at DATETIME,
-website_session_id BIGINT ,
-primary_product_id BIGINT ,
-items_purchased BIGINT ,
-price_usd DECIMAL(6, 2) ,
-cogs_usd DECIMAL(6, 2) ,
-PRIMARY KEY (order_id)
+CREATE TABLE orders (
+    order_id BIGINT,
+    created_at DATETIME,
+    website_session_id BIGINT,
+    primary_product_id BIGINT,
+    items_purchased BIGINT,
+    price_usd DECIMAL(6 , 2 ),
+    cogs_usd DECIMAL(6 , 2 ),
+    PRIMARY KEY (order_id)
 );
 
 
@@ -36,32 +36,23 @@ ORDER BY order_id ;
 
 
 -- Setting up AUTOMATION to update orders table whenever order_items table got a new data;  
-CREATE TRIGGER insert_new_orders
-AFTER INSERT ON order_items
-FOR EACH ROW
-REPLACE INTO orders
-SELECT 
-      order_id,
-      MIN(created_at) AS created_at,
-      MIN(website_session_id) AS website_session_id,
-      SUM(CASE
-          WHEN is_primary_item = 1 THEN product_id
-          ELSE NULL 
-          END) AS primary_product_id,
-      COUNT(order_item_id) AS items_purchased,
-      ROUND(SUM(price_usd), 2) AS price_usd,
-      ROUND(SUM(cogs_usd), 2) AS cogs_usd
-FROM order_items
-WHERE order_id =new.order_id
-GROUP BY order_id
-ORDER BY order_id ;
-      
-/* order_items table was populated and it Trigger the load of orders table. 
-orders table was 10033 rows and increased to 12036 after order_items was updated */
+CREATE 
+    TRIGGER  insert_new_orders
+ AFTER INSERT ON order_items FOR EACH ROW 
+    REPLACE INTO orders SELECT order_id,
+        MIN(created_at) AS created_at,
+        MIN(website_session_id) AS website_session_id,
+        SUM(CASE
+            WHEN is_primary_item = 1 THEN product_id
+            ELSE NULL
+        END) AS primary_product_id,
+        COUNT(order_item_id) AS items_purchased,
+        ROUND(SUM(price_usd), 2) AS price_usd,
+        ROUND(SUM(cogs_usd), 2) AS cogs_usd FROM
+        order_items
+    WHERE
+        order_id = new.order_id
+    GROUP BY order_id
+    ORDER BY order_id;
 
-
--- Records BEFORE INSERT INTO order_items : 10,033 rows
--- Records AFTER INSERT INTO order_items : 12,036 rows
--- BEFORE Max(created_at): 2013-12-31 23:22:54
--- AFTER Max(created_at):  2014-02-28 23:40:45
 
