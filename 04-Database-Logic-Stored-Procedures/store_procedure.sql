de396858c8cd6501da2224b4fb3cd68597f5e9c7
Ayodeji <aayo.agunbiade@gmail.com>
2025-11-26 20:09:29 +0100

    Store Procedure Logic

---
+++ 04-Database-Logic-Stored-Procedures/store_procedure.sql
@@ -0,0 +1,31 @@
+
+-- Order Performance
+DELIMITER //
+CREATE PROCEDURE order_performance (IN startdate DATE, enddate DATE)
+BEGIN
+SELECT COUNT(order_id) AS total_orders,
+       SUM(price_usd) AS total_revenue
+FROM orders
+WHERE DATE(created_at) BETWEEN startdate AND enddate ;       
+END //
+DELIMITER ;
+
+
+
+-- Total Films and Store Data
+DELIMITER //
+CREATE PROCEDURE total_film_and_store (IN firstname VARCHAR(60), lastname VARCHAR(60))
+BEGIN
+SELECT store_id, COUNT(DISTINCT film_id) AS total_film FROM inventory_non_normalized
+WHERE store_manager_first_name = firstname AND store_manager_last_name = lastname 
+GROUP BY store_id;
+END //
+DELIMITER ;
+
+CALL total_film_and_store ('Jon', 'Stephens');
+
+
+
+
+
+
